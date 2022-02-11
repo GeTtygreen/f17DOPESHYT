d46abad36295a36276da5ebd2215fe521bfbb401
GeTtygreen <94148141+GeTtygreen@users.noreply.github.com>
2022-02-11 14:25:10 -0600

    updated after turn in

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,40 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test('that draw button displaysthe div with id =choices', async () => {
+    const drawBtn = await driver.findElement(By.id('draw '))
+
+    await driver.sleep(3000)
+
+    await drawBtn.click()
+
+    
+    const choiceSection = await driver.findElement(By.id('choices'))
+    
+    const isDisplayed = await choiceSection.isDisplayed();
+    
+    await driver.sleep(3000)
+
+    expect(isDisplayed).toBe(true)
+})
+
+test('"Add to Duo"button display the id ="player-id"',async () => {
+    await driver.navigate().refresh();
+
+    const drawBtn = await driver.findElement(By.id('draw'));
+
+    await driver.sleep(3000)
+    await drawBtn.click();
+    
+    const  addToDuoBtns = await driver.findElements(By.xpath('//button[text()="Add to Duo"]'));
+    
+    await driver.sleep(3000)
+    await addToDuoBtns[1].click();
+    await driver.sleep(3000)
+
+    const isDisplayed = await driver.findElement(By.id('player-duo'))
+
+    expect(isDisplayed).toBe(false)
+
+})
